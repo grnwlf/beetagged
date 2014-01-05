270e8c78326034c708c7cdb7d1ad40e0dfffd719
Chris O'Neil <cconeil5@gmail.com>
2014-01-05 00:30:20 -0500

    turned off auto layout in the storyboard added the top part of a contact’s view

--- Bees Chest/ContactViewController.h
+++ Bees Chest/ContactViewController.h
@@ -8,8 +8,14 @@
 
 #import <UIKit/UIKit.h>
 #import "Contact.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 @interface ContactViewController : UIViewController
 
 @property (nonatomic, strong) Contact *contact;
+@property (strong, nonatomic) IBOutlet UIImageView *contactImage;
+@property (strong, nonatomic) IBOutlet UILabel *nameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+@property (strong, nonatomic) IBOutlet UILabel *companyLabel;
+@property (strong, nonatomic) IBOutlet UIButton *goToLinkedInButton;
 
 @end
--- Bees Chest/ContactViewController.m
+++ Bees Chest/ContactViewController.m
@@ -26,24 +26,71 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	NSLog(@"Contact = %@", self.contact);
+    // remove all of the autolayout contraints because the fuck everything up
+    [self.view removeConstraints:self.view.constraints];
+    
+    // 1. set the image
+	[self.contactImage setImageWithURL:[NSURL URLWithString:self.contact.pictureUrl] placeholderImage:kContactCellPlaceholderImage];
+    
+    // 2.set the Contact name
+    self.nameLabel.text = self.contact.formattedName;
+    
+    // 3. set the Contact's job title and the Contact's position title
+    if (self.contact.positionTitle && self.contact.positionTitle.length > 0 && self.contact.positionName && self.contact.positionName.length > 0) {
+        self.companyLabel.text = [NSString stringWithFormat:@"at %@",self.contact.positionName];
+        self.titleLabel.text = self.contact.positionTitle;
+    } else if (self.contact.positionTitle && self.contact.positionTitle.length > 0) {
+        self.companyLabel.text = @"";
+        self.titleLabel.text = self.contact.positionTitle;
+        float moveBy = 10.0;
+        
+        // 1. move the name label down
+        CGRect f = self.nameLabel.frame;
+        [self.nameLabel setFrame:CGRectMake(f.origin.x, f.origin.y + moveBy, f.size.width, f.size.height)];
+        
+        // 2. move the position label down
+        f = self.titleLabel.frame;
+        [self.titleLabel setFrame:CGRectMake(f.origin.x, f.origin.y + moveBy, f.size.width, f.size.height)];
+        
+        // 3. move the button up
+        f = self.goToLinkedInButton.frame;
+        [self.goToLinkedInButton setFrame:CGRectMake(f.origin.x, f.origin.y - moveBy, f.size.width, f.size.height)];
+
+
+    } else {
+        self.companyLabel.text = @"";
+        self.titleLabel.text = @"";
+        
+        float moveBy = 30.0;
+        // 1. move the name label down
+        CGRect f = self.nameLabel.frame;
+        [self.nameLabel setFrame:CGRectMake(f.origin.x, f.origin.y + moveBy, f.size.width, f.size.height)];
+        
+        // 2. move the button up
+        f = self.goToLinkedInButton.frame;
+        [self.goToLinkedInButton setFrame:CGRectMake(f.origin.x, f.origin.y - moveBy, f.size.width, f.size.height)];
+    }
+    self.goToLinkedInButton.backgroundColor = [UIColor yellowColor]; // starfish make a better color
 }
 
-- (void)viewDidAppear:(BOOL)animated
-{
+
+- (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     NSLog(@"contact view did appear");
 }
 
-- (void)viewWillAppear:(BOOL)animated
-{
+- (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     NSLog(@"showing contact view");
     self.navigationController.navigationBar.hidden = NO;
 }
 
-- (void)didReceiveMemoryWarning
-{
+// function that opens the linkedIn profile
+- (void)goToLinkedIn {
+    NSLog(@"Implement go to linkedIn!!!");
+}
+
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
